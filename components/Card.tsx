18925f87761b856d587868f0db15144073efaebb
muhammadazharalgifari <azhargifari22@gmail.com>
2025-10-03 15:19:32 +0700

    fix: styling card car for mobile

--- components/Card.tsx
+++ components/Card.tsx
@@ -15,7 +15,7 @@ const Card = ({ car }: { car: Car }) => {
           className="w-full h-full object-cover rounded-t-lg"
         />
       </div>
-      <div className="p-8 space-y-2">
+      <div className="p-6 md:p-8 space-y-2">
         <h4 className="text-lg font-semibold text-gray-600 uppercase">
           <div className="flex items-center justify-between">
             <Link
@@ -34,9 +34,11 @@ const Card = ({ car }: { car: Car }) => {
         </h4>
         <div className="flex items-center justify-between">
           <div className="flex items-center space-x-2">
-            <div className="flex items-center space-x-2 bg-green-100 text-green-700 p-2.5 rounded-full">
-              <Users className="w-4 h-4" />
-              <span className="font-medium text-xs">{car.capacity} seats</span>
+            <div className="flex items-center space-x-2 bg-green-100 text-green-700 p-2 md:p-2.5 rounded-full">
+              <Users className="w-3 h-3 md:w-4 md:h-4" />
+              <span className="font-medium text-[10px] md:text-xs">
+                {car.capacity} seats
+              </span>
             </div>
             <div className="flex items-center space-x-2 bg-gray-100 p-2 rounded-full">
               <span className="font-normal text-xs">{car.year}</span>
@@ -45,9 +47,9 @@ const Card = ({ car }: { car: Car }) => {
 
           <Link
             href={`/cars/detail/${car.id}`}
-            className="px-6 py-2.5 flex items-center space-x-2 font-semibold text-white group bg-gradient-to-r from-indigo-500 to-indigo-800 hover:from-indigo-600 hover:to-indigo-900 rounded-lg transition-all duration-300 hover:scale-105 hover:shadow-lg"
+            className="px-3 py-1.5 md:py-2.5 flex items-center space-x-1 md:space-x-2 text-xs md:text-sm font-semibold text-white group bg-gradient-to-r from-indigo-500 to-indigo-800 hover:from-indigo-600 hover:to-indigo-900 rounded-lg transition-all duration-300 hover:scale-105 hover:shadow-lg"
           >
-            <Eye className="w-5 h-5 group-hover:-translate-x-1 transition-transform" />
+            <Eye className="w-4 h-4 md:w-5 md:h-5 group-hover:-translate-x-0.5 transition-transform" />
             <span>View Detail</span>
           </Link>
         </div>
